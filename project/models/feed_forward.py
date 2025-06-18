5e7510a53cc732ef7d11fa533d1a35d267f29e68
S9runf <davideluccioli@gmail.com>
2025-06-18 21:25:06 +0200

    added norm layer

--- project/models/feed_forward.py
+++ project/models/feed_forward.py
@@ -1,21 +1,22 @@
 import torch.nn as nn
 
 class FeedForward(nn.Module):
-    def __init__(self, input_dim, output_dim, critic=False):
+    def __init__(self, input_dim, output_dim):
         super().__init__()
-        self.critic = critic
+        self.norm = nn.LayerNorm(input_dim)
         self.shared = nn.Sequential(
             nn.Linear(input_dim, 128),
             nn.Tanh(),
-            nn.Linear(128, 128),
+            nn.Linear(128, 256),
             nn.Tanh(),
-            nn.Linear(128, 64),
+            nn.Linear(256, 128),
             nn.Tanh()
         )
 
-        self.out = nn.Linear(64, output_dim)
+        self.out = nn.Linear(128, output_dim)
 
     def forward(self, input):
-        x = self.shared(input)
-        return self.out(x)  
+        x = self.norm(input)
+        x = self.shared(x)
+        return self.out(x)
     
--- project/utils.py
+++ project/utils.py
@@ -2,72 +2,45 @@ from overcooked_ai_py.mdp.overcooked_mdp import OvercookedGridworld
 from overcooked_ai_py.mdp.overcooked_env import OvercookedEnv, Overcooked
 import random
 
-def make_env(layout_name, linear_features=True):
+def make_env(layout_name, rew_shaping=None):
     """ Create an Overcooked environment from a layout name """
 
     base_mdp = OvercookedGridworld.from_layout_name(
         layout_name,
         old_dynamics=True,
+        params_to_overwrite={
+            "rew_shaping_params": rew_shaping
+        }
     )
     base_env = OvercookedEnv.from_mdp(base_mdp, horizon=400, info_level=0)
 
-    if linear_features:
-        featurize_fn = base_env.featurize_state_mdp
-    else:
-        featurize_fn = base_env.lossless_state_encoding_mdp
     return Overcooked(
         base_env=base_env,
-        featurize_fn=featurize_fn
+        featurize_fn=base_env.featurize_state_mdp
     )
 
 
-class CurriculumStage:
-    def __init__(self, layouts, linear_features=True):
+class GeneralizedOvercooked:
+    def __init__(self, layouts):
         self.envs = []
 
         assert isinstance(layouts, list), "Layouts should be a list of layout names."
         assert len(layouts) > 0, "At least one layout name must be provided."
 
         for layout in layouts:
-            env = make_env(layout, linear_features=linear_features)
+            env = make_env(layout, rew_shaping=None)
             self.envs.append(env)
-
-class GeneralizedOvercooked:
-    def __init__(self, curriculum=None, layouts=None, linear_features=True):
-        if curriculum is None and layouts is None:
-            raise ValueError("Either curriculum or layouts must be provided.")
-        
-        if curriculum and layouts:
-            raise ValueError("Only one of curriculum or layouts should be provided.")
-
-        if curriculum:
-            self.stages = [
-                CurriculumStage(**stage, linear_features=linear_features)
-                  for stage in curriculum
-            ]
-        elif layouts:
-            self.layouts = layouts
-            for layout in layouts:
-                self.stages = [
-                    CurriculumStage([layout],    linear_features=linear_features)
-                ]
-
-        self.stage_idx = 0
-        self.curr_env = self.stages[self.stage_idx].envs[0]
-        self.observation_space = self.curr_env.observation_space
-        self.action_space = self.curr_env.action_space
+        self.cur_env = self.envs[0]
+        self.observation_space = self.cur_env.observation_space
+        self.action_space = self.cur_env.action_space
 
     def reset(self):
-        self.curr_env = random.choice(self.stages[self.stage_idx].envs)
-        return self.curr_env.reset()
+        idx = random.randint(0, len(self.envs)-1)
+        self.cur_env = self.envs[idx]
+        return self.cur_env.reset()
 
     def step(self, *args):
-        return self.curr_env.step(*args)
+        return self.cur_env.step(*args)
 
-    def next_stage(self):
-        if self.stage_idx < len(self.stages) - 1:
-            self.stage_idx += 1
-            self.curr_env = self.stages[self.stage_idx].envs[0]
-            print(f"Moved to stage {self.stage_idx + 1}.")
-        else:
-            raise IndexError("No more stages available in the curriculum.")
+    def render(self, *args):
+        return self.cur_env.render(*args)
